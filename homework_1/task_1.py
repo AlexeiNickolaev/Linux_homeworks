b74fee1c6e1325f6aa5f516bb02c8c81f4cdae30
Alexei <datlife2015@yandex.ru>
2023-09-23 08:29:17 +0300

    homework_1

---
+++ homework_1/task_1.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+def text_in_command_output(command, text_to_find):
+    try:
+        output = subprocess.check_output(command, shell=True,
+                                         universal_newlines=True)
+        return text_to_find in output
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
+
+
+command_to_execute = "ls"
+text_to_search = "nginx.list"
+result = text_in_command_output(command_to_execute, text_to_search)
+print(result)
